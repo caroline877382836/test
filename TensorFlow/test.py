455038529222d519eceb86f89a6fb5dec0d7b059
Xuan <CNXuaCar@elekta.com>
2017-12-22 17:43:15 +0800

    change test file about exchange system

--- TensorFlow/test.py
+++ TensorFlow/test.py
@@ -1,27 +1,85 @@
-# import tensorflow as tf
-# 
-# hello = tf.constant('hello,tensorflow')
-# sess = tf.Session()
-# print(sess.run(hello))
+from enum import Enum
 
+class operation(Enum):
+    BUY = 1
+    SELL = 2
+    MODIFY = 3
+    CANCEL = 4
+    PRINT = 5
+    
 class ExchangeSystem():
-    def __init__(self,name): 
-        self._log_file_name = name
-        self._log_file = open(self._log_file_name, "w")
-        
-    def _buy(self):    
-        
-        return 0
     
-    def _sell(self):    
-        
-        return 0
+    def __init__(self, line_str): 
+        self._input = line_str.split()
+        if self._input[0] == operation.BUY.value:
+            return 0
+            
+    def _SetOrderType(self,orderType): 
+        self._orderType = orderType
     
-    def _modify(self):    
+    def _SetPrice(self, price): 
+        self._price = price
         
-        return 0
+    def _SetNewPrice(self, new_price): 
+        self._NewPrice = new_price
+                
+    def _SetQuantity(self,quantity): 
+        self._quantity = quantity
+        
+    def _SetNewQuantity(self,new_quantity): 
+        self._NewQuantity = new_quantity
+        
+    def _SetOrderID(self,orderID): 
+        self._orderID = orderID
     
-    def _cancel(self):    
+    def _SetModifyOpe(self,newOpe): 
+        self._newOperation = newOpe   
+           
+    def _GetOrderType(self): 
+        return self._orderType
+    
+    def _GetPrice(self): 
+        return self._price
+    
+    def _GetNewPrice(self): 
+        return self._NewPrice
+        
+    def _GetQuantity(self): 
+        return self._quantity
+    
+    def _GetNewQuantity(self): 
+        return self._NewQuantity
+        
+    def _GetOrderID(self): 
+        return self._orderID
+    
+    def _GetModifyOpe(self): 
+        return self._newOperation
+        
+         
+    def _IniBUY(self): 
+        ls_input = self._input   
+        self._SetOrderType(ls_input[0])
+        self._SetPrice(ls_input[1])
+        self._SetQuantity(ls_input[2])
+        self._SetOrderID(ls_input[3])        
+    
+    def _IniSELL(self):    
+        ls_input = self._input   
+        self._SetOrderType(ls_input[0])
+        self._SetPrice(ls_input[1])
+        self._SetQuantity(ls_input[2])
+        self._SetOrderID(ls_input[3])         
+    
+    def _IniModify(self):    
+        ls_input = self._input  
+        self._SetOrderID(ls_input[0]) 
+        self._SetModifyOpe(ls_input[1])
+        self._SetNewPrice(ls_input[2])
+        self._SetNewQuantity(ls_input[3])
+         
+    
+    def _IniCancel(self):    
         
         return 0 
     
